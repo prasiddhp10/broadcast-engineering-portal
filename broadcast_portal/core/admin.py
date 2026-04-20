46ee573c180fe8a486f5f8dd1081f2e3ece93d26
prasiddhp10 <prasiddhapanta74@gmail.com>
2026-04-20 09:00:00 +0545

    Registering models in admin of core

--- broadcast_portal/core/admin.py
+++ broadcast_portal/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import AuditLog
 # Register your models here.
+
+@admin.register(AuditLog)
+
+class AuditLogAdmin(admin.ModelAdmin):
+    list_display = ['timestamp', 'user', 'action', 'entity_type', 'entity_id']
+    list_filter = ['action', 'entity_type']
+    readonly_fields = ['timestamp']
